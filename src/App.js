8b1db5a24f7edfc0f585899789c2aca515bf05fb
Landy1000 <74451238+Landy1000@users.noreply.github.com>
2021-02-27 01:23:58 +0900

    learn prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <h1>Hello,world</h1>
+import PropTypes from 'prop-types';
+
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10},
+    { name: "Taro", age: 5},
+  ]
+  return (
+  <div>
+  {
+    profiles.map((profile, index) => {
+      return <User name={profile.name} age={profile.age} key={index} />
+    })
+  }
+  </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
